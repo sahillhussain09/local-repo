69417ad2317a63ff2faa29453db98a92ac56cbcf
sahillhussain09 <sh7389764@email.com>
2023-10-16 17:26:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".btn");
+const nameSpan = document.querySelector(".username");
+
+
+btn.addEventListener(onclick, () => {
+    console.log("hello world");
+    nameSpan.innerHTML = "sahil hussain"
+})
